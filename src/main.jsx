fbbbb95b855f9899f5549adfc526c649db2cbf8d
Ariful2634 <80531678+Ariful2634@users.noreply.github.com>
2023-09-23 20:12:14 +0600

    navbar done

---
+++ src/main.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Root/Root';
+import Home from './Home/Home';
+import Favourites from './Header/Favourites/Favourites';
+import Login from './Login/Login';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Root></Root>,
+    children:[
+      {
+        path:'/home',
+        element:<Home></Home>
+      },
+      {
+        path:'/fav',
+        element:<Favourites></Favourites>
+      },
+      {
+        path:'/login',
+        element:<Login></Login>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <RouterProvider router={router} />
+  </React.StrictMode>,
+)
